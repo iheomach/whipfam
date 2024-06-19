7052fce3615d94390c97df70bfbe097a408714a1
iheomach <iheomach@ualberta.ca>
2024-06-19 10:27:09 -0600

    fixed blank by importing order in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import Navbar from './components/Menu/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import SC from './pages/SC/SC'
 import Home from './pages/Home/Home'
+import Order from './pages/Order/Order'
 
 const App = () => {
   return (
     <div className = 'app'>
       <Navbar/>
       <Routes>
-        <Route path='/Home' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/SC' element={<SC/>}/>
         <Route path='/Order' element={<Order/>}/>
       </Routes>
